72cfd3812aada04615ce365e357a1ea98235fda7
Emma <emma.campan98@gmail.com>
2019-11-19 14:19:24 +0100

    Added table service

---
+++ KarmaSushi/Implementation/TableServices.cs
@@ -0,0 +1,32 @@
+﻿using Interface;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implementation
+{
+    class TableService : ITable
+    {
+        public Table InsertTable(Table table)
+        {
+            try
+            {   //Todo table controller
+                using (var instance = new TableController())
+                {
+                    return instance.GetOrderById(id);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new FaultException<Error>(new Error()
+                {
+                    CodigoError = "10001",
+                    Mensaje = ex.Message,
+                    Description = "Exception managed by the administrator"
+                });
+            }
+        }
+    }
