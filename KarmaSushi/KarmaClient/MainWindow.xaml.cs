0b8cc99196b2dce30306f3a0f94b282fbf8f6e39
Mark Lauridsen <the.mark.lauridsen@gmail.com>
2019-11-19 16:01:24 +0100

    guiWIP
    Started gui work

---
+++ KarmaSushi/KarmaClient/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using KarmaClient.OrderServiceRef;
+
+namespace KarmaClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ///private readonly List<Button> _MenuItems = new List<Button>;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            OrderServiceClient client = new OrderServiceClient();
+
+            System.Console.WriteLine("test");
+          
+
+            MenuPanel.Children.Add(new Button() { Content = "test" });
+            MenuPanel.Children.Add(new Button() { Content = client.GetOrderById("5").Price.ToString() });
+
+        }
+    }
+}
