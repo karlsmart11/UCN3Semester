c5a6d68c1d316b407f1ae24e2affc27943c2c6fc
Emma Campan <emma.campan98@gmail.com>
2019-11-20 09:34:56 +0100

    table stuff

---
+++ KarmaSushi/Controller/TableController.cs
@@ -0,0 +1,28 @@
+﻿using Interface;
+using Model;
+using SQLRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controller
+{
+    class TableController : IDisposable
+    {
+        public Table InsertTable (Table table)
+        {
+            ITable instance = new TableRepository();
+            return instance.InsertTable(table);
+
+        }
+
+
+
+        void IDisposable.Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
