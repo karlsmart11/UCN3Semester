0c26a00a485cc23048de9b7a7f8fec909a646534
Stenberg <mat_elm13@hotmail.com>
2019-11-20 11:59:33 +0100

    To Mark

--- KarmaSushi/KarmaClient/MainWindow.xaml.cs
+++ KarmaSushi/KarmaClient/MainWindow.xaml.cs
@@ -24,8 +24,62 @@ namespace KarmaClient
             
             foreach (var currentOrder in client.GetAllOrders())
             {
-                MenuPanel.Children.Add(new Button { Content = new Button { Content = currentOrder.Time } });
-            }
+                MenuPanel.Children.Add(new Button { Content = new Button { Content = currentOrder.Time, FontSize = 8} });
+            }        
+        }
+
+        //Create menu button object
+        private Button CreateButton(Product product)
+        {
+            var rows = new Grid
+            {
+                RowDefinitions = { new RowDefinition(), new RowDefinition(), new RowDefinition()},
+                
+            };
+
+            var columns = new Grid
+            {
+                ColumnDefinitions = {new ColumnDefinition(), new ColumnDefinition()},
+
+            };
+            
+            rows.Children.Add(columns);
+            Grid.SetRow(columns, 2);
+
+            var namelbl = new Label();
+            rows.Children.Add(namelbl);
+            Grid.SetRow(namelbl, 0);
+            namelbl.Content = product.Name;
+
+            var descriptionlbl = new Label();
+            rows.Children.Add(descriptionlbl);
+            Grid.SetRow(descriptionlbl, 1);
+            descriptionlbl.Content = product.Description;
+
+            var pricelbl = new Label();
+            columns.Children.Add(pricelbl);
+            Grid.SetColumn(pricelbl, 0);
+            descriptionlbl.Content = product.Price;
+
+            var b = new Button
+            {
+                Content = rows
+            };
+
+            b.Click += RoutedEventHandler(addToOrderList);
+
+            return b;
+        }
+
+        ///Adds menu item to order list by id
+        private void addToOrderList(object sender, RoutedEventArgs e)
+        {
+            
+            return;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
             
         }
     }
