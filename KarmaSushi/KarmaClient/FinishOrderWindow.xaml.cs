6107fa146e9658d161307b4f1802018223dd0646
Hutihati <mat_elm13@hotmail.com>
2019-11-26 12:57:29 +0100

    Table lists behavior updated

--- KarmaSushi/KarmaClient/FinishOrderWindow.xaml.cs
+++ KarmaSushi/KarmaClient/FinishOrderWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Windows;
 using KarmaClient.OrderServiceRef;
 using KarmaClient.EmployeeServiceRef;
