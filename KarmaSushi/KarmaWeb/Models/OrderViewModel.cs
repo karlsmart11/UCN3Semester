d65a639b4f1c623cec65a80ebdda071bd172fe0a
Hutihati <mat_elm13@hotmail.com>
2019-12-03 13:41:56 +0100

    Beautified the ordering page. Utilizing forms

---
+++ KarmaSushi/KarmaWeb/Models/OrderViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using KarmaWeb.OrderServiceRef;
+using KarmaWeb.ProductServiceRef;
+using Product = KarmaWeb.ProductServiceRef.Product;
+
+namespace KarmaWeb.Models
+{
+    public class OrderViewModel
+    {
+        public List<Product> Products { get; set; }
+        public List<OrderLine> OrderLines { get; set; }
+        public string  OrderComment { get; set; }
+        public double TotSum { get; set; }
+        public double Sum { get; set; }
+        public Order CurrentOrder { get; set; }
+        public Product Product { get; set; }
+    }
+}
